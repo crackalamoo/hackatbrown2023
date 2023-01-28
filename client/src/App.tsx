20095f2f9049213e5e2be88fceda250888234f86
jameshu111 <boo15869@gmail.com>
2023-01-28 17:38:44 -0500

    webcam and 3 pictures

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,12 @@
 import React, { useEffect, useRef } from "react";
+import Webcam from "react-webcam";
 import "./App.css";
+import Biocaptcha from "./components/biocaptcha/Biocaptcha";
 
 function App() {
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  useEffect(() => {
-    getVideo();
-  }, [videoRef]);
-
-  const getVideo = () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: { width: 300 } })
-      .then((stream) => {
-        let video = videoRef.current;
-        if (video) {
-          video.srcObject = stream;
-          video.play();
-        }
-      })
-      .catch((err) => console.error("error:", err));
-  };
-
-  const paintToCanvas = () => {
-    let video = videoRef.current;
-  };
-
   return (
     <div>
-      <button>Take a photo</button>
-      <video ref={videoRef} />
+      <Biocaptcha />
     </div>
   );
 }
