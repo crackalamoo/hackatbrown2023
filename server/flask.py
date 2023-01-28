51bab49d8fee420f361cb9a175a00fd9c4a88a8a
crackalamoo <harysdalvi@gmail.com>
2023-01-28 16:13:34 -0500

    started verification backend

--- server/flask.py
+++ server/flask.py
@@ -5,6 +5,18 @@ app = Flask(__name__, static_folder='../client')
 cors = CORS(app, resources={r"/api/*": {"origins": ["http://localhost:3000"]}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/api/verify_webcam')
-def verify(webcam_data):
-    pass
+def checkWebcam(webcam_data):
+    # TODO: Check if webcam data is valid
+    return True
+
+@app.route('/secret')
+def secret(webcam_data):
+    return verify(webcam_data, {"secret": "You are a verified human!"})
+
+def verify(webcam_data, secret):
+    if checkWebcam(webcam_data):
+        return jsonify(secret)
+    return jsonify({})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
