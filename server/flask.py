f04497f47955360f5ebcbd8fdd0866f9215a946d
Chirag Furia <chiragfuria@gmail.com>
2023-01-28 16:22:38 -0500

    opencv

--- server/flask.py
+++ server/flask.py
@@ -2,15 +2,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import cv2 as cv
 
-
-
 app = Flask(__name__, static_folder='../client')
 cors = CORS(app, resources={r"/api/*": {"origins": ["http://localhost:3000"]}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
 def checkWebcam(webcam_data):
-    grayscale_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    grayscale_image = cv.cvtColor(webcam_data, cv.COLOR_BGR2GRAY)
     detected_faces = face_cascade.detectMultiScale(grayscale_image)
     return len(detected_faces) == 1
 
